2fd1ad38d058d61c07dad01c5992e6fb2fca7673
lalomx <lalo0892@gmail.com>
2022-03-21 08:26:24 -0600

    express

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const router = require("express").Router()
 
 router.get("/", (req, res) => res.send("OK"))
+router.get("/:id", (req, res) => res.send("OK"))
+router.post("/popular", (req, res) => res.send("OK"))
+router.put("/", (req, res) => res.send("OK"))
+router.delete("/", (req, res) => res.send("OK"))
 
 module.exports = router
